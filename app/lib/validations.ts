bf02b549787301927d2ac59b62e9669716972d63
sktomsi <tomcy48@gmail.com>
2024-04-28 18:34:28 +0530

    change validations for folder name and description

--- app/lib/validations.ts
+++ app/lib/validations.ts
@@ -1,8 +1,8 @@
 import { z } from 'zod';
 
 export const CreateFolderSchema = z.object({
-  name: z.string().min(1).max(20),
-  description: z.string().max(30).optional(),
+  name: z.string().min(1).max(255),
+  description: z.string().max(255).optional(),
   emoji: z.string().emoji('You can only add emojis').min(1).max(250),
 });
 
