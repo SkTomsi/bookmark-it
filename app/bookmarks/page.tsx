64377ee3273a73cc88f515e2f2cb8bace1fc53c4
Tomcy Thomas <tomcy48@gmail.com>
2024-05-08 16:38:12 +0530

    add dynamic

--- app/bookmarks/page.tsx
+++ app/bookmarks/page.tsx
@@ -1,6 +1,8 @@
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import { redirect } from 'next/navigation';
 
+export const dynamic = 'force-dynamic';
+
 export default async function BoookmarksPage() {
   const { isAuthenticated } = getKindeServerSession();
 
